a69cd7197753c5ad0a1b4bc0ccd8da9ecf9751df
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-08-03 18:21:21 +0530

    Fix: Empty Habits

--- MCR-2/src/components/HabitsListing.jsx
+++ MCR-2/src/components/HabitsListing.jsx
@@ -3,11 +3,21 @@ import SingleHabit from "./SingleHabit";
 
 const HabitsListing = ({ habits, archive }) => {
   return (
-    <div className="grid grid-cols-3 gap-3 gap-y-4 mb-14 max-[800px]:grid-cols-2 max-[500px]:grid-cols-1">
-      {habits?.map((habit) => {
-        return <SingleHabit key={habit?.id} habit={habit} archive={archive} />;
-      })}
-    </div>
+    <>
+      {habits?.length === 0 ? (
+        <p className="text-center text-3xl font-bold mt-10">
+          No Habits found...
+        </p>
+      ) : (
+        <div className="grid grid-cols-3 gap-3 gap-y-4 mb-14 max-[800px]:grid-cols-2 max-[500px]:grid-cols-1">
+          {habits?.map((habit) => {
+            return (
+              <SingleHabit key={habit?.id} habit={habit} archive={archive} />
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 };
 
